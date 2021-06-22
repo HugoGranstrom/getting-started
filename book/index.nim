a0f3ae28378ce32560f87374d5c806aeb9269c3d
Hugo Granström <5092565+HugoGranstrom@users.noreply.github.com>
2021-06-22 22:37:39 +0200

    Update index.nim

--- book/index.nim
+++ book/index.nim
@@ -6,6 +6,8 @@ nbUseNimibook
 nbText: """
 # Introduction
 
+COOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLOOOOOOOOOOOOOOOOOOOLLLLLLLLLLLLLLLLLLLL B)
+
 This is the SciNim getting started book.
 
 It's a collection of examples and code snippet to help people use the scientific libraries of the Nim programming language.
